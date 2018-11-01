0b67a5af89e7df05ede28779c7f3e883bd421220
Andriy-Tatarchuk <37184328+Andriy-Tatarchuk@users.noreply.github.com>
2018-11-02 01:10:21 +0200

    Updated Exam view

--- Shell/ViewModel/ExamDictionaryViewModel.cs
+++ Shell/ViewModel/ExamDictionaryViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using CommonServiceLocator;
 using Enigma.Data;
 using Enigma.Entity.Entities;
 using Enigma.Shell.Navigation;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
 using GalaSoft.MvvmLight.Command;
 
 namespace Enigma.Shell.ViewModel
@@ -55,51 +60,54 @@ namespace Enigma.Shell.ViewModel
             }
         }
 
-        private string _TestValue1;
-        public string TestValue1
-        {   
-            get { return _TestValue1; }
-            set 
+        private Answer _Answer1;
+        public Answer Answer1
+        {
+            get { return _Answer1; }
+            set
             {
-                _TestValue1 = value;
-                RaisePropertyChanged("TestValue1");
+                _Answer1 = value;
+                RaisePropertyChanged("Answer1");
             }
         }
 
-        private string _TestValue2;
-        public string TestValue2
+        private Answer _Answer2;
+        public Answer Answer2
         {
-            get { return _TestValue2; }
-            set 
+            get { return _Answer2; }
+            set
             {
-                _TestValue2 = value;
-                RaisePropertyChanged("TestValue2");
+                _Answer2 = value;
+                RaisePropertyChanged("Answer2");
             }
         }
 
-        private string _TestValue3;
-        public string TestValue3
+        private Answer _Answer3;
+        public Answer Answer3
         {
-            get { return _TestValue3; }
-            set 
+            get { return _Answer3; }
+            set
             {
-                _TestValue3 = value;
-                RaisePropertyChanged("TestValue3");
+                _Answer3 = value;
+                RaisePropertyChanged("Answer3");
             }
         }
 
-        private string _TestValue4;
-        public string TestValue4
+        private Answer _Answer4;
+        public Answer Answer4
         {
-            get { return _TestValue4; }
-            set 
+            get { return _Answer4; }
+            set
             {
-                _TestValue4 = value;
-                RaisePropertyChanged("TestValue4");
+                _Answer4 = value;
+                RaisePropertyChanged("Answer4");
             }
         }
+
+        private List<Answer> Answers { get; set; }
 
         public RelayCommand NextWordCommand { get; private set; }
+        public RelayCommand<string> ShowAnswerCommand { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the DictionariesViewModel class.
@@ -108,7 +116,25 @@ namespace Enigma.Shell.ViewModel
             : base(dataMgr, navigationService)
         {
             NextWordCommand = new RelayCommand(NextWord);
+            ShowAnswerCommand = new RelayCommand<string>(ShowAnswer);
+            Answers = new List<Answer>();
         }
+
+        private void ShowAnswer(string tag)
+        {
+            var index = 0;
+            if (Int32.TryParse(tag, out index))
+            {
+                if (!Answers[index].IsCorrect)
+                {
+                    Answers[index].Background = Brushes.Red;
+                }
+
+                Answers.FirstOrDefault(a => a.IsCorrect).Background = Brushes.Green;
+            }
+        }
+
+        
 
         public override async Task Navigated(object param)
         {
@@ -142,7 +168,8 @@ namespace Enigma.Shell.ViewModel
             Words.Shuffle();
             CurrentWordIndex = 0;
             CurrentWord = null;
-            TestValue1 = TestValue2 = TestValue3 = TestValue4 = String.Empty;
+            Answers.Clear();
+            Answer1 = Answer2 = Answer3 = Answer4 = null;
 
             NextWord();
         }
@@ -153,24 +180,28 @@ namespace Enigma.Shell.ViewModel
             {
                 CurrentWord = Words[CurrentWordIndex];
                 var rnd = new Random();
-                var indexes = new List<int>();
+                Answers.Clear();
                 for (int i = 0; i < 4; )
                 {
                     var index = rnd.Next(0, Words.Count);
-                    if (!indexes.Contains(index))
+                    if (!Answers.Any(a=> a.Text == Words[index].Translation))
                     {
-                        indexes.Add(index);
+                        Answers.Add(new Answer(Words[index].Translation));
                         i++;
                     }
                 }
-                if (!indexes.Contains(CurrentWordIndex))
+                if (!Answers.Any(a=> a.Text == Words[CurrentWordIndex].Translation))
                 {
-                    indexes[rnd.Next(0, 4)] = CurrentWordIndex;
+                    Answers[rnd.Next(0, 4)] = new Answer(Words[CurrentWordIndex].Translation){IsCorrect = true};
                 }
-                TestValue1 = Words[indexes[0]].Translation;
-                TestValue2 = Words[indexes[1]].Translation;
-                TestValue3 = Words[indexes[2]].Translation;
-                TestValue4 = Words[indexes[3]].Translation;
+                else
+                {
+                    Answers.FirstOrDefault(a => a.Text == Words[CurrentWordIndex].Translation).IsCorrect = true;
+                }
+                Answer1 = Answers[0];
+                Answer2 = Answers[1];
+                Answer3 = Answers[2];
+                Answer4 = Answers[3];
                 CurrentWordIndex++;
             }
         }
@@ -191,6 +222,42 @@ namespace Enigma.Shell.ViewModel
                 list[k] = list[n];
                 list[n] = value;
             }
+        }
+    }
+
+    public class Answer : INotifyPropertyChanged
+    {
+        public bool IsCorrect { get; set; }
+        public string Text { get; set; }
+        private Brush _Background;
+
+        public Brush Background
+        {
+            get { return _Background; }
+            set
+            {
+                _Background = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Answer()
+        {
+            IsCorrect = false;
+        }
+
+        public Answer(string text)
+        {
+            Text = text;
+            IsCorrect = false;
+            Background = Brushes.Gray;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
